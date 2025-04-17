9fa58f97a47bc4b0bd4d0abe6663958f098c19d3
Alejotamayo28 <alejandropucca0@gmail.com>
2025-04-17 00:11:11 -0500

    fixxing x2

--- _worker.js
+++
@@ -1,9 +0,0 @@
-import { createRequestHandler } from '@cloudflare/next-on-pages'
-export default {
-  async fetch(request, env) {
-    return createRequestHandler({
-      // Asegúrate de que coincida con tu estructura
-      buildId: 'wbjqH9YlwVC-kRjH9Qc6r', // Revisa en .next/BUILD_ID
-    })(request)
-  }
-}
